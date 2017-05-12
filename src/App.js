c422c4c562d245b1ad4ec59c435e752e030caa40
Luans <luansnain@gmail.com>
2017-05-12 23:16:26 +0200

    Home, login, signup pages

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import Routes from './Routes';
+import { Navbar, Nav } from 'react-bootstrap';
+import RouteNavItem from './components/RouteNavItem';
+import './App.css';
+
+class App extends Component {
+
+  handleNavLink = (event) => {
+    event.preventDefault();
+    this.props.history.push(event.currentTarget.getAttribute('href'));
+  }
+
+  render() {
+    return (
+      <div className="App container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">Nassh</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <RouteNavItem onClick={this.handleNavLink} href="/signup">Signup</RouteNavItem>
+              <RouteNavItem onClick={this.handleNavLink} href="/login">Login</RouteNavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Routes />
+      </div>
+    );
+  }
+}
+
+export default withRouter(App);
